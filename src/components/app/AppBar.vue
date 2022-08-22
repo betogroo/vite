907db62eda91e883a86049d19699f6a21fb741cb
betogroo <luizhumberto@gmail.com>
2022-08-22 09:17:06 -0300

    chore: install unplugin-vue-components

---
+++ src/components/app/AppBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center">
+    <router-link :to="{ name: 'HomeView' }">Home</router-link> |
+    <router-link :to="{ name: 'AboutView' }">About</router-link>
+  </div>
+</template>
